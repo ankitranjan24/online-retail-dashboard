388271e0998f8be243f6e4b7525ad7880bc0fd77
Dheeraj <dheerajsoni351@gmail.com>
2021-06-01 15:25:25 +0530

    Updates

---
+++ precache-manifest.845d2fa94e076b9cd6b82e07c5a044ac.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5b85488404f9c3055ee",
+    "url": "/moneyu-react-app-master/static/css/main.f6badf0f.chunk.css"
+  },
+  {
+    "revision": "a5b85488404f9c3055ee",
+    "url": "/moneyu-react-app-master/static/js/main.a5b85488.chunk.js"
+  },
+  {
+    "revision": "e039a8bad2dba804a354",
+    "url": "/moneyu-react-app-master/static/css/1.ee59141c.chunk.css"
+  },
+  {
+    "revision": "e039a8bad2dba804a354",
+    "url": "/moneyu-react-app-master/static/js/1.e039a8ba.chunk.js"
+  },
+  {
+    "revision": "3e5709fdc4d6d664e488",
+    "url": "/moneyu-react-app-master/static/js/runtime~main.3e5709fd.js"
+  },
+  {
+    "revision": "be2ed1511f856f57648ebb4d1c0a1b96",
+    "url": "/moneyu-react-app-master/static/media/user-img-placeholder.be2ed151.jpeg"
+  },
+  {
+    "revision": "4fcdd97fadc3a1d9887f816f2aa67f1d",
+    "url": "/moneyu-react-app-master/static/media/CircularStd-Medium.4fcdd97f.otf"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/moneyu-react-app-master/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/moneyu-react-app-master/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "d6ff69c82cc37b323ceb5ff20224cc99",
+    "url": "/moneyu-react-app-master/static/media/fa-brands-400.d6ff69c8.woff"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/moneyu-react-app-master/static/media/fa-brands-400.c6aef942.ttf"
+  },
+  {
+    "revision": "ae990e80be9a9904db60b0d3d06adbc1",
+    "url": "/moneyu-react-app-master/static/media/fa-brands-400.ae990e80.woff2"
+  },
+  {
+    "revision": "065edea411671c8caceecb999354cc21",
+    "url": "/moneyu-react-app-master/static/media/fa-regular-400.065edea4.eot"
+  },
+  {
+    "revision": "1010b72339fe3292aede3543e827b741",
+    "url": "/moneyu-react-app-master/static/media/fa-brands-400.1010b723.eot"
+  },
+  {
+    "revision": "2e562d4ebf15395f00bc738738f79291",
+    "url": "/moneyu-react-app-master/static/media/fa-regular-400.2e562d4e.ttf"
+  },
+  {
+    "revision": "9c0eb6c2e967eccd837da618bcbde91c",
+    "url": "/moneyu-react-app-master/static/media/fa-regular-400.9c0eb6c2.woff2"
+  },
+  {
+    "revision": "87f37798dcdf11a68de16eacfd566aa3",
+    "url": "/moneyu-react-app-master/static/media/fa-regular-400.87f37798.woff"
+  },
+  {
+    "revision": "5dc39cf0aed41e1a5e9918bea7e8603d",
+    "url": "/moneyu-react-app-master/static/media/fa-regular-400.5dc39cf0.svg"
+  },
+  {
+    "revision": "a6de637ca793ed8092597012d691a6a0",
+    "url": "/moneyu-react-app-master/static/media/fa-solid-900.a6de637c.woff"
+  },
+  {
+    "revision": "52134b924fd61958f88323845deffc64",
+    "url": "/moneyu-react-app-master/static/media/fa-solid-900.52134b92.woff2"
+  },
+  {
+    "revision": "a20e6745adc3184de8330b1f0579161f",
+    "url": "/moneyu-react-app-master/static/media/fa-solid-900.a20e6745.eot"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/moneyu-react-app-master/static/media/fa-solid-900.872545dd.ttf"
+  },
+  {
+    "revision": "c95f5f7a8923b8bb5034de9eb48901d7",
+    "url": "/moneyu-react-app-master/static/media/fa-brands-400.c95f5f7a.svg"
+  },
+  {
+    "revision": "56404b18f3d1a3eaeed35b43343cfab9",
+    "url": "/moneyu-react-app-master/static/media/fa-solid-900.56404b18.svg"
+  },
+  {
+    "revision": "c648425ffdb72e6b055e02c52ffb1b6b",
+    "url": "/moneyu-react-app-master/index.html"
+  }
+];
